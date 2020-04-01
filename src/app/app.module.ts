21a9775c318336084a172fb59df60352146ccb53
EmotionFever <pedromar2010@gmail.com>
2020-04-01 23:40:27 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookInstanceListComponent } from './book-instance-list/book-instance-list.component';
 import { BookInstanceDetailComponent } from './book-instance-detail/book-instance-detail.component';
 import { AuthorCreateComponent } from './author-create/author-create.component';
-import { BookCreateComponent } from './book-create/book-create.component';
 
 @NgModule({
   imports:      [ 
@@ -26,7 +25,7 @@ import { BookCreateComponent } from './book-create/book-create.component';
     AppRoutingModule, 
     HttpClientModule,
     ],
-  declarations: [ AppComponent, GenreListComponent, DashboardComponent, GenreCreateComponent, GenreDetailComponent, AuthorListComponent, AuthorDetailComponent, BookListComponent, BookDetailComponent, BookInstanceListComponent, BookInstanceDetailComponent, AuthorCreateComponent, BookCreateComponent ],
+  declarations: [ AppComponent, GenreListComponent, DashboardComponent, GenreCreateComponent, GenreDetailComponent, AuthorListComponent, AuthorDetailComponent, BookListComponent, BookDetailComponent, BookInstanceListComponent, BookInstanceDetailComponent, AuthorCreateComponent],
   bootstrap:    [ AppComponent ],
   providers: [LibraryRequestsService]
 })

7b233c3747f4abe9180e07f50c7716b18161b24d
EmotionFever <pedromar2010@gmail.com>
2020-04-01 12:42:27 +0100

    4 files modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LibraryRequestsService } from '../library-requests.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  title;
+  error;
+  data;
+
+  constructor(private request: LibraryRequestsService) { }
 
   ngOnInit() {
+    this.showData();
+  }
+
+  showData() {
+    this.request.getNet("")
+      .subscribe((data: Object) => {
+        this.title = data['title'];
+        const secondKey = Object.keys(data)[1]; //fetched the key at second index
+        this.error = data[secondKey];
+        const thirdKey = Object.keys(data)[2]; //fetched the key at second index
+        this.data = data[thirdKey];
+      });
   }
 
 }
